0c6ac1d0842819003fc65e4a36694e0b3c7c1f90
gu-geonhoe <rjsghl92@gmail.com>
2022-09-14 14:07:47 +0900

    v1

---
+++ src/main/java/com/example/Api/HelloController.java
@@ -0,0 +1,31 @@
+package com.example.Api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+// 1.편비티아이 결과로 인한 카테고리별 상품 1개 씩 총 11개
+// 2. 베스트 추천 리뷰
+    @Operation(summary = "", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@Parameter(description = "이름", required = true, example = "Park") @RequestParam String name) {
+        return ResponseEntity.ok("hello " + name);
+    }
+
+    @PostMapping("/hello")
+    public String hello2( String key) {
+        return key;
+    }
+
+}
---
+++ src/main/java/com/example/Api/UserController.java
@@ -0,0 +1,16 @@
+package com.example.Api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class UserController {
+    // 1. 로그인 ,
+    // 2. 회원가입
+    // 3. 유저 상세 페이지
+    // 4. 유저 정보 수정
+    // 5. 유저 삭제 (회원 탈퇴)
+
+}
