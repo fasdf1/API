e0850da92108e17eb1ebb93f3ade65efbff23171
gu-geonhoe <rjsghl92@gmail.com>
2022-09-16 00:41:22 +0900

    멤버 , 카테고리

--- src/main/java/com/example/Api/member/memberController.java
+++ src/main/java/com/example/Api/member/memberController.java
@@ -1,10 +1,15 @@
 package com.example.Api.member;
 
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -12,23 +17,49 @@ import java.util.List;
 public class memberController {
 
 
-    // 1. 로그인 ,
-    // 2. 회원가입
-    // 3. 유저 상세 페이지
-    // 4. 유저 정보 수정
+
     // 5. 유저 삭제 (회원 탈퇴)
 
-    @PostMapping
-    public ResponseEntity signup(Member member) {
+
+//    @PostMapping("/login")// 로그인 로직 구현 예정
+//    public ResponseEntity login(Member member){
+//        return new ResponseEntity<> (member,HttpStatus.CREATED);
+//    }
+
+    @PostMapping // 회원가입
+    @ApiOperation(value = "회원가입")
+    public ResponseEntity signup(@Validated @RequestBody Member member) {
 
 
         return new ResponseEntity<Member>(member, HttpStatus.OK);
     }
+    @PatchMapping("/{member-id}")
+    @ApiOperation(value = "회원 정보 수정")// 회원 정보 수정  -- 구체적인 수정 추가 예정
+    public ResponseEntity update(@PathVariable("member-id") @Positive long Id,
+                                 @Valid @RequestBody Member member){
 
-    @GetMapping()
-    public ResponseEntity userpage(){
-        Member member1 = new Member(1,"멤버네임","이메일","1234");
+return new ResponseEntity<>(member, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{member-id}")
+    @ApiOperation(value = "마이 페이지")// 유저 상세 페이지
+    public ResponseEntity memberPage(@PathVariable("member-id") @Positive long Id){
+//        Member member1 = new Member(1,"멤버네임","이메일","1234");
         Member member2 = new Member();
-        return new ResponseEntity<>(member1,HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(member2,HttpStatus.ACCEPTED);
+    }
+
+//    @GetMapping("/all") // 모든 유저 조회
+//    public ResponseEntity memberall(@Positive @RequestParam int page){
+//        Page<Member> page1 =
+//        return new ResponseEntity<>(page1,HttpStatus.ACCEPTED);
+//    }
+
+    @DeleteMapping("/{member-id}")
+    @ApiOperation(value = "회원탈퇴")//회원 탈퇴
+    public ResponseEntity memberDelete(@PathVariable("member-id") @Positive long Id){
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 }
