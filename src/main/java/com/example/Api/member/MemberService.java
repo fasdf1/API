438b5fdd1e0920bb211ab809d84c2b7743afa848
gu-geonhoe <rjsghl92@gmail.com>
2022-09-16 19:19:10 +0900

    8

--- src/main/java/com/example/Api/member/MemberService.java
+++ src/main/java/com/example/Api/member/MemberService.java
@@ -1,7 +1,14 @@
 package com.example.Api.member;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 }
